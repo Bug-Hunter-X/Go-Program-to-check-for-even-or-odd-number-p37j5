bf72e9f68c7d5e28e4bd6f154acd89ea09df708e
BugHunter-X <askzenai@gmail.com>
2024-12-10 13:18:06 +0100

    Add bug.go file

---
+++ bug.go
@@ -0,0 +1,14 @@
+func main() {
+
+
+        fmt.Println("Enter a number")
+        var a int
+        fmt.Scanln(&a)
+
+        if a%2 == 0 {
+                fmt.Println("Even")
+        } else {
+                fmt.Println("Odd")
+        }
+
+}
